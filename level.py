8e39ac0ef6cd452f9fc4ad852f7ebaeda906db01
Georgios Rizothanasis <trivialgeorge@gmail.com>
2023-11-29 21:47:24 +0100

    Add CameraGroup class

--- level.py
+++ level.py
@@ -9,7 +9,7 @@ class Level:
         self.display_surface = pygame.display.get_surface()
 
         # sprite groups
-        self.all_sprites = pygame.sprite.Group()
+        self.all_sprites = CameraGroup()
 
         self.setup()
         self.overlay = Overlay(self.player)
@@ -19,7 +19,17 @@ class Level:
 
     def run(self, dt):
         self.display_surface.fill("black")
-        self.all_sprites.draw(self.display_surface)
+        # self.all_sprites.draw(self.display_surface)
+        self.all_sprites.custom_draw()
         self.all_sprites.update(dt)
 
         self.overlay.display()
+
+class CameraGroup(pygame.sprite.Group):
+    def __init__(self):
+        super().__init__()
+        self.display_surface = pygame.display.get_surface()
+
+    def custom_draw(self):
+        for sprite in self.sprites():
+            self.display_surface.blit(sprite.image, sprite.rect)
