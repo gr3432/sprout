4676315e0d69c8614b83c0a5a930839f764a066b
Georgios Rizothanasis <trivialgeorge@gmail.com>
2023-12-01 09:13:59 +0100

    Add player inventory

--- level.py
+++ level.py
@@ -46,7 +46,12 @@ class Level:
 
         # trees
         for obj in tmx_data.get_layer_by_name("Trees"):
-            Tree((obj.x, obj.y), obj.image, [self.all_sprites, self.collision_sprites, self.tree_sprites], obj.name)
+            Tree(
+                pos = (obj.x, obj.y), 
+                surf = obj.image, 
+                groups = [self.all_sprites, self.collision_sprites, self.tree_sprites], 
+                name = obj.name,
+                player_add = self.player_add)
 
         # world
         Generic(
@@ -68,6 +73,9 @@ class Level:
                     collision_sprites = self.collision_sprites,
                     tree_sprites = self.tree_sprites)
 
+    def player_add(self, item):
+        self.player.item_inventory[item] += 1
+
     def run(self, dt):
         self.display_surface.fill("black")
         # self.all_sprites.draw(self.display_surface)
@@ -75,6 +83,7 @@ class Level:
         self.all_sprites.update(dt)
 
         self.overlay.display()
+        print(self.player.item_inventory)
 
 class CameraGroup(pygame.sprite.Group):
     def __init__(self):
--- sprites.py
+++ sprites.py
@@ -56,7 +56,7 @@ class Particle(Generic):
             self.kill()
 
 class Tree(Generic):
-    def __init__(self, pos, surf, groups, name):
+    def __init__(self, pos, surf, groups, name, player_add):
         super().__init__(pos, surf, groups)
 
         # tree attributes
@@ -72,6 +72,8 @@ class Tree(Generic):
         self.apple_sprites = pygame.sprite.Group()
         self.create_fruit()
 
+        self.player_add = player_add
+
     def damage(self):
         # damage tree
         self.health -= 1
@@ -80,6 +82,7 @@ class Tree(Generic):
         if len(self.apple_sprites.sprites()) > 0:
             random_apple = choice(self.apple_sprites.sprites())
             Particle(random_apple.rect.topleft, random_apple.image, self.groups()[0], LAYERS['fruit'])
+            self.player_add('apple')
             random_apple.kill()
 
     def check_death(self):
@@ -95,6 +98,7 @@ class Tree(Generic):
             self.rect = self.image.get_rect(midbottom = self.rect.midbottom)
             self.hitbox = self.rect.copy().inflate(-10, -self.rect.height * 0.6)
             self.alive = False
+            self.player_add('wood')
 
     def update(self, dt):
         if self.alive:
