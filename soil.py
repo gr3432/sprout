e8485c4d637f8a3c2459a04823608c69437f28d2
Georgios Rizothanasis <trivialgeorge@gmail.com>
2023-12-03 10:36:27 +0100

    Add soil tile class

--- soil.py
+++ soil.py
@@ -2,6 +2,13 @@ import pygame
 from settings import *
 from pytmx.util_pygame import load_pygame
 
+class SoilTile(pygame.sprite.Sprite):
+    def __init__(self, pos, surf, groups):
+        super().__init__(groups)
+        self.image =surf
+        self.rect = self.image.get_rect(topleft = pos)
+        self.z = LAYERS["soil"]
+
 class SoilLayer:
     def __init__(self, all_sprites):
         # sprite groups
@@ -9,7 +16,7 @@ class SoilLayer:
         self.soil_sprites = pygame.sprite.Group()
 
         # graphichs
-        self.soil_surface = pygame.image.load("../graphics/soil/o.png")
+        self.soil_surf = pygame.image.load("../graphics/soil/o.png")
 
         self.create_soil_grid()
         self.create_hit_rects()
@@ -39,4 +46,14 @@ class SoilLayer:
                 y = rect.y // TILE_SIZE
 
                 if 'F' in self.grid[y][x]:
-                    print('farmable')
+                    self.grid[y][x].append('X')
+                    self.create_soil_tiles()
+
+    def create_soil_tiles(self):
+        self.soil_sprites.empty()
+        for index_row, row in enumerate(self.grid):
+            for index_col, cell in enumerate(row):
+                if 'X' in cell:
+                    x = index_col * TILE_SIZE
+                    y = index_row * TILE_SIZE
+                    SoilTile((x, y), self.soil_surf, [self.all_sprites, self.soil_sprites])
