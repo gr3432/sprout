d75e6abef86423b76dbc2f5db3222d78dbe082e5
Georgios Rizothanasis <trivialgeorge@gmail.com>
2023-12-04 20:46:15 +0100

    Add water soil by rain logic

--- soil.py
+++ soil.py
@@ -59,6 +59,8 @@ class SoilLayer:
                 if 'F' in self.grid[y][x]:
                     self.grid[y][x].append('X')
                     self.create_soil_tiles()
+                    if self.raining:
+                        self.water_all()
 
     def water(self, target_pos):
         for soil_sprite in self.soil_sprites.sprites():
@@ -70,6 +72,15 @@ class SoilLayer:
 
                 # create water sprite
                 WaterTile(soil_sprite.rect.topleft, choice(self.water_surfs), [self.all_sprites, self.water_sprites])
+
+    def water_all(self):
+        for index_row, row in enumerate(self.grid):
+            for index_col, cell in enumerate(row):
+                if 'X' in cell and 'W' not in cell:
+                    cell.append('W')
+                    x = index_col * TILE_SIZE
+                    y = index_row * TILE_SIZE
+                    WaterTile((x, y), choice(self.water_surfs), [self.all_sprites, self.water_sprites])
 
     def remove_water(self):
         # destroy all water sprites
