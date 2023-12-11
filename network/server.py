04033fee57aa0d2e36aaf966c23d8da81a7a40a8
Georgios Rizothanasis <trivialgeorge@gmail.com>
2023-12-11 21:43:13 +0100

    Add threaded client

--- network/server.py
+++ network/server.py
@@ -1,7 +1,7 @@
 import socket
 from _thread import start_new_thread
 
-server = ""
+server = "192.168.1.149"
 port = 5555
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -15,10 +15,24 @@ s.listen(2)
 print("Waiting for connection, Server started")
 
 def threaded_client(conn):
-    pass
+    reply = ""
+    while True:
+        try:
+            data = conn.recv(2048)
+            reply = data.decode("utf-8")
+
+            if not data:
+                print("Disconnected")
+                break
+            else:
+                print("Received: ", reply)
+                print("Sending: ", reply)
+                conn.sendall(str.encode(reply))
+        except:
+            break
 
 while True:
     conn, addr = s.accept()
-    print("Connected to:", addr)
+    print("Connected to: ", addr)
 
     start_new_thread(threaded_client, (conn,))
