6018ef12682788af255a8dad4acbabecb38b3a95
Georgios Rizothanasis <trivialgeorge@gmail.com>
2023-12-14 20:22:04 +0100

    Move player to separate module

--- network/client.py
+++ network/client.py
@@ -1,5 +1,6 @@
 import pygame
 from network import Network
+from player import Player
 
 width = 500
 height = 500
@@ -9,36 +10,7 @@ pygame.display.set_caption("Client")
 
 clientNumber = 0
 
-class Player:
-    def __init__(self, x, y, width, height, color):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-        self.color = color
-        self.rect = (x, y, width, height)
-        self.vel = 3
 
-    def draw(self, win):
-        pygame.draw.rect(win, self.color, self.rect)
-
-    def move(self):
-        keys = pygame.key.get_pressed()
-
-        if keys[pygame.K_LEFT]:
-            self.x -= self.vel
-        if keys[pygame.K_RIGHT]:
-            self.x += self.vel
-        if keys[pygame.K_UP]:
-            self.y -= self.vel
-        if keys[pygame.K_DOWN]:
-            self.y += self.vel
-
-        self.update()
-
-
-    def update(self):
-        self.rect = (self.x, self.y, self.width, self.height)
 
 def read_pos(data):
     data = data.split(",")
