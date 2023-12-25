b2f5d066131fd4fdf6920e0b65c83ab48bf769b7
Georgios Rizothanasis <trivialgeorge@gmail.com>
2023-12-25 14:00:52 +0100

    Add client side

---
+++ with_select/client.py
@@ -0,0 +1,10 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # ipv4, tcp
+
+# since client runs on the same machine we can use gethostname for ip
+# but usually it should be the ip of the server
+s.connect((socket.gethostname(), 1234)) 
+
+msg = s.recv(1024) # chunk of data, byte stream
+print(msg.decode("utf-8"))
