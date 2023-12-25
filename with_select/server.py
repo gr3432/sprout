8abfe2c705a34456e49b9d6647bf5445609fa0f4
Georgios Rizothanasis <trivialgeorge@gmail.com>
2023-12-25 13:55:41 +0100

    Add server side

--- with_select/server.py
+++ with_select/server.py
@@ -1,3 +1,13 @@
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # ipv4, tcp
+
+s.bind((socket.gethostname(), 1234)) # ip, port
+
+s.listen(5) # queue of connections
+
+while True:
+    clientsocket, address = s.accept()
+    print(f"Connection from {address} has been established!")
+
+    clientsocket.send(bytes("Welcome to the server!", "utf-8"))
