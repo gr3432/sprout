b40e2908148537cbcea5df254f808763404b3166
Georgios Rizothanasis <trivialgeorge@gmail.com>
2023-12-27 15:58:15 +0100

    Add server socket

--- with_select/server.py
+++ with_select/server.py
@@ -3,3 +3,11 @@ import socket
 HEADER_LENGTH = 10
 IP = "127.0.0.1"
 PORT = 1234
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# to be able to reusue the port
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+server_socket.bind((IP, PORT))
+
+server_socket.listen()
