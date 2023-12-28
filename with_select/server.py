585a44b57713ec4ce84b03cb828c23c088361f2c
Georgios Rizothanasis <trivialgeorge@gmail.com>
2023-12-28 17:19:17 +0100

    Add receive message on server

--- with_select/server.py
+++ with_select/server.py
@@ -11,3 +11,20 @@ server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server_socket.bind((IP, PORT))
 
 server_socket.listen()
+
+sockets_list = [server_socket]
+
+clients = {}
+
+def receive_message(client_socket):
+    try:
+        message_header = client_socket.recv(HEADER_LENGTH)
+
+        if not len(message_header):
+            return False
+        
+        message_length = int(message_header.decode("utf-8").strip())
+        return {"header": message_header, "data": client_socket.recv(message_length)}
+    
+    except:
+        return False
