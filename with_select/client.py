0eeca957c288d60fc7f64562cb5701ad29fc3396
Georgios Rizothanasis <trivialgeorge@gmail.com>
2023-12-29 11:43:56 +0100

    Receive messages

--- with_select/client.py
+++ with_select/client.py
@@ -1,4 +1,5 @@
 import socket
+import sys
 
 HEADER_LENGTH = 10
 IP = "127.0.0.1"
@@ -21,3 +22,21 @@ while True:
         message_header = f"{len(message):<{HEADER_LENGTH}}".encode("utf-8")
         
         client_socket.send(message_header +  message)
+    
+    try:
+        while True:
+            # receive messages
+            username_header = client_socket.recv(HEADER_LENGTH)
+            if not len(username_header):
+                print("connection closed by the server")
+                sys.exit()
+            username_length = int(username_header.decode("utf-8").strip())
+            username = client_socket.recv(username_length).decode("utf-8")
+
+            message_header = client_socket.recv(HEADER_LENGTH)
+            message_length = int(message_header.decode("utf-8").strip())
+            message = client_socket.recv(message_length).decode("utf-8")
+
+            print(f"{username} > {message}")
+    except:
+        pass
