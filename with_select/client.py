25a17e8762058a85c6eff0d3b0668217e09486f3
Georgios Rizothanasis <trivialgeorge@gmail.com>
2023-12-29 11:36:18 +0100

    Send message

--- with_select/client.py
+++ with_select/client.py
@@ -13,3 +13,11 @@ username = my_username.encode("utf-8")
 username_header = f"{len(username):<{HEADER_LENGTH}}".encode("utf-8")
 client_socket.send(username_header + username)
 
+while True:
+    message = input(f"{my_username} > ")
+
+    if message:
+        message = message.encode("utf-8")
+        message_header = f"{len(message):<{HEADER_LENGTH}}".encode("utf-8")
+        
+        client_socket.send(message_header +  message)
