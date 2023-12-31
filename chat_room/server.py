85ea0f6a35b499febb9d689e4867cfd3051be58d
Georgios Rizothanasis <trivialgeorge@gmail.com>
2023-12-31 11:25:16 +0100

    Add server functions

--- chat_room/server.py
+++ chat_room/server.py
@@ -6,3 +6,25 @@ port = 7895
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(host, port)
 server.listen()
+
+
+clients = []
+nicknames = []
+
+def broadcast(message):
+    for client in clients:
+        client.send(message)
+
+def handle(client):
+    while True:
+        try:
+            message = client.recv(1024)
+            broadcast(message)
+        except:
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            nickname = nicknames[index]
+            broadcast(f"{nickname} left the chat".encode("utf-8"))
+            nicknames.remove(nickname)
+            break
