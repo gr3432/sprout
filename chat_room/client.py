49dfa54a838c0621569ce4ea7cfe3ef620ab2e3d
Georgios Rizothanasis <trivialgeorge@gmail.com>
2024-01-05 20:40:48 +0100

    Add client nickname handling

--- chat_room/client.py
+++ chat_room/client.py
@@ -3,6 +3,8 @@ import socket
 host = "127.0.0.1"
 port = 7895
 
+nickname = input("Choose a nickname: ")
+
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 client.connect((host, port))
@@ -12,7 +14,7 @@ def receive():
         try:
             message = client.recv(1024).decode("utf-8")
             if message == "NICK":
-                pass
+                client.send(nickname.encode("utf-8"))
             else:
                 print(message)
         except:
