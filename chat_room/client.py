6bd3420697d25b38421d5e10386fc51fd8bfad7c
Georgios Rizothanasis <trivialgeorge@gmail.com>
2024-01-05 20:38:13 +0100

    Add client receive

--- chat_room/client.py
+++ chat_room/client.py
@@ -6,3 +6,16 @@ port = 7895
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 client.connect((host, port))
+
+def receive():
+    while True:
+        try:
+            message = client.recv(1024).decode("utf-8")
+            if message == "NICK":
+                pass
+            else:
+                print(message)
+        except:
+            print("An error occurred!")
+            client.close()
+            break
